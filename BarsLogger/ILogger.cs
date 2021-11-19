00e1289baae6d5e7d9389bbc1dfcba569fd541d8
neronorai <vitkit99@gmail.com.com>
2021-11-19 22:29:16 +0300

    build_test_24

--- BarsLogger/ILogger.cs
+++ BarsLogger/ILogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace BarsLogger
 {
-    /// <summary>Интерфейс работы с логjv</summary>
+    /// <summary>Интерфейс работы с логом</summary>
     public interface ILog
     {
         /// <summary>
