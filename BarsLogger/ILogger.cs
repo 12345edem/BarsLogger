da76cdab0243f02546bfb9564510a788e3b874ac
neronorai <44476217+neronorai@users.noreply.github.com>
2021-11-22 23:30:47 +0300

    [#1] Изменение комментария
    Тестовое описание коммита

--- BarsLogger/ILogger.cs
+++ BarsLogger/ILogger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace BarsLogger
 {
-    /// <summary>Интерфейс работы с логами</summary>
+    /// <summary>Интерфейс работы с логом</summary>
     public interface ILog
     {
         /// <summary>
